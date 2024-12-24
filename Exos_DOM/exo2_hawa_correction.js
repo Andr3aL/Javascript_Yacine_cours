f5fe49537d4b12cc0c0cbde1284d768e39a647dc
Andr3aL <aleo759291@gmail.com>
2024-12-24 10:08:01 +0100

    24-12-24 a terrage a 10h07

--- Exos_DOM/exo2_hawa_correction.js
+++ Exos_DOM/exo2_hawa_correction.js
@@ -12,46 +12,7 @@ toggleRectangle.addEventListener('click', ()=>{
     rectangle.classList.toggle('rectangle');
 });
 
-// let divInfo = document.querySelector("#info");
 
-// divInfo.addEventListener("mouseover", function(){
-//     divInfo.style.backgroundColor = "gold";
-//     divInfo.style.borderRadius = "50%";
-//     divInfo.textContent = "GOLD";    
-// });
-
-toggleRectangle.addEventListener('mouseover', ()=>{
-    // rectangle.classList.toggle('rectangle');
-    
-});
-
-toggleRectangle.addEventListener('mouseout', ()=>{
-    // rectangle.classList.toggle('rectangle');
-});
-
-let rectangle3 = document.querySelector(".rectangle3");
-
-rectangle3.addEventListener('mouseenter', ()=>{
-    rectangle3.style.backgroundColor = "red";
-});
-
-// let rectangle3 = document.querySelector(".rectangle3");
-
-// toggleRectangle.addEventListener('mouseleave', ()=>{
-//     rectangle.style.backgroundColor = "blue";
-// });
-
-let rectangle2 = document.querySelector(".rectangle2");
-
-rectangle2.addEventListener('mouseleave', ()=>{
-    rectangle2.style.backgroundColor = "blue";
-});
-
-let rectangle4 = document.querySelector(".rectangle4");
-
-rectangle4.addEventListener('dblclick', ()=>{
-    rectangle3.style.backgroundColor = "green";
-});
 
 // Autre façon de faire : 
 
